d1efae0c27470643f1dfa39a1578dca7dfe3f22c
AC Harkness <perilouspaper@gmail.com>
2023-02-25 16:40:16 -0500

    scoring naming

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -105,10 +105,11 @@ public class GameManager : MonoBehaviour
             //    playerWinOrLose = "lose";
             //}
             trialNum = trialNum + 1;
-            Tinylytics.AnalyticsManager.LogCustomMetric("trialName", save2Initials.roundName+"_"+trialName);
-            Tinylytics.AnalyticsManager.LogCustomMetric("Computer Score", save2Initials.roundName + "_" + _computerScore.ToString());
-            Tinylytics.AnalyticsManager.LogCustomMetric("Total Score", save2Initials.roundName + "_" + _totalScore.ToString());
-            Tinylytics.AnalyticsManager.LogCustomMetric("Player Score", save2Initials.roundName + "_" + _playerScore.ToString());
+          //  Tinylytics.AnalyticsManager.LogCustomMetric("trialName", save2Initials.roundName+"_"+trialName);
+            Tinylytics.AnalyticsManager.LogCustomMetric(save2Initials.roundName + "_" + trialName + "_" + trialNum.ToString(), _computerScore + "_" + _playerScore + "_" + _totalScore);
+         //  Tinylytics.AnalyticsManager.LogCustomMetric("Computer Score", save2Initials.roundName + "_" + _computerScore.ToString());
+         //   Tinylytics.AnalyticsManager.LogCustomMetric("Total Score", save2Initials.roundName + "_" + _totalScore.ToString());
+          //  Tinylytics.AnalyticsManager.LogCustomMetric("Player Score", save2Initials.roundName + "_" + _playerScore.ToString());
             //Tinylytics.AnalyticsManager.LogCustomMetric("Left Wall Misses Above Racket ",  ballStats.L_missCounter_Over.ToString());
             //Tinylytics.AnalyticsManager.LogCustomMetric("Left Wall Misses Above Racket ", ballStats.L_missCounter_Under.ToString());
             //Debug.Log("misses over " + ballStats.L_missCounter_Over);
